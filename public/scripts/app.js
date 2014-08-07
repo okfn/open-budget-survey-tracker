6c2bea05f19537da12af195374b70f23a5c25285
John Martin <me@johnmart.in>
2014-08-07 15:12:49 +0100

    Adds search field to table

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,37 @@
 !function ($) {
+
+  var timeout;
+  var value;
+  var input;
+
+  function FilterTable () {
+    value = $.trim(value).toLowerCase();
+    if (value) {
+      $('#overview-table tbody tr').hide();
+      var to_show = $('#overview-table tbody tr[data-search*="'+value+'"]');
+      if (to_show.length > 0) {
+        to_show.show();
+        $('#overview-table tbody .empty').hide();
+      } else {
+        $('#overview-table tbody .empty').show();
+      }
+    } else {
+      $('#overview-table tbody tr').show();
+    }
+  }
+
+  function UpdateHistory () {
+    if (typeof history !== 'undefined') {
+      var params = ( value ) ? '/?'+$.param({ "q": value }) : '/';
+      history.pushState(null, null, params);
+    }
+    FilterTable();
+  }
+
   $(function () {
     var modal = $('#modal');
     $('[data-toggle="tooltip"]').tooltip();
+
     $('#overview-table tbody tr').on('click', function(event) {
       event.preventDefault();
       var json = $(this).data('json');
@@ -15,5 +45,26 @@
       modal.modal();
     });
     $('#overview-table').fixedHeader({ topOffset: 0 });
+
+    input = $('#search-form input[name="q"]');
+    $('#search-form').on('submit', function (event) {
+      event.preventDefault();
+      clearTimeout(timeout);
+      value = input.val();
+      UpdateHistory();
+    });
+    input.on('keyup', function() {
+      value = input.val();
+      clearTimeout(timeout);
+      timeout = setTimeout(UpdateHistory, 250);
+    });
+
+    // OK if there's a value here...
+    value = input.val();
+    if (value) {
+      FilterTable();
+    }
+    $(window).on('popstate', FilterTable);
+
   });
 }(window.jQuery);
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res) {
   api.call('overview', function (countries) {
     res.render('index', {
+      'q': req.param('q'),
       'countries': countries
     });
   });
