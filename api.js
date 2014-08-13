37f34981cd408dd989766ad9b3dd03e367992ef6
John Martin <me@johnmart.in>
2014-08-13 14:58:58 +0100

    Adds really simple caching to the api

--- api.js
+++ api.js
@@ -1,15 +1,43 @@
 var request = require('request');
+var fs = require('fs');
+
 var uri = 'http://aquarium-staging.herokuapp.com/';
+// Sets the cache age to an hour
+var cache_age = 3600000;
 
 function api_call (endpoint, callback) {
-  var url = uri+endpoint+'.json';
-  request(url, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      callback(JSON.parse(body));
-    } else {
-      console.log(error);
+  var cache_file = './cache/'+endpoint+'.json';
+  var should_get_from_cache = false;
+  var should_update_cache = true;
+  var cache_exists = fs.existsSync(cache_file);
+  if (cache_exists) {
+    should_update_cache = false;
+    should_get_from_cache = true;
+    var stat = fs.statSync(cache_file);
+    var difference = new Date().getTime() - stat.mtime.getTime();
+    console.log(difference);
+    if (difference > cache_age) {
+      should_update_cache = true;
+      should_get_from_cache = false;
     }
-  });
+  }
+  if (should_get_from_cache) {
+    console.log(1);
+    var data = fs.readFileSync(cache_file);
+    callback(JSON.parse(data));
+  } else {
+    var url = uri+endpoint+'.json';
+    request(url, function (error, response, data) {
+      if (!error && response.statusCode == 200) {
+        if (should_update_cache) {
+          fs.writeFileSync(cache_file, data);
+        }
+        callback(JSON.parse(data));
+      } else {
+        console.log(error);
+      }
+    });
+  }
 }
 
 module.exports = {
